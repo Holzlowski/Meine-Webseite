3b8465a93ac76f5728d102529d557be041b4a397
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2025-07-16 18:28:36 +0200

    Add two new skill cards and enhance 3D card carousel
    Added 'Programmieren.png' and 'Äpfel3D.jpg' images as new skill cards. Updated App.js to include five cards in the 3D carousel and adjusted the import order. Enhanced App.css to support five cards, improved card styling, shadows, and z-index animations for a smoother 3D effect.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Container } from 'react-bootstrap';
 import NavigationBar from './components/Navigation/Navbar';
 import avatarImage from './assets/images/Avatar.png'; // Beispiel für ein Avatar-Bild
-import karte1 from './assets/images/skillkarten/Alchemielabor.png';
-import karte2 from './assets/images/skillkarten/Wüstensand.png';
-import karte3 from './assets/images/skillkarten/GravityPlanet.png';
+import karte1 from './assets/images/skillkarten/Wüstensand.png';
+import karte2 from './assets/images/skillkarten/Alchemielabor.png';
+import karte3 from './assets/images/skillkarten/Äpfel3D.jpg';
+import karte4 from './assets/images/skillkarten/GravityPlanet.png';
+import karte5 from './assets/images/skillkarten/Programmieren.png';
 import './App.css';
 
 function App() {
@@ -22,6 +24,8 @@ function App() {
           <div className="card-item card-1"><img src={karte1} alt="Karte1" /></div>
           <div className="card-item card-2"><img src={karte2} alt="Karte2" /></div>
           <div className="card-item card-3"><img src={karte3} alt="Karte3" /></div>
+          <div className="card-item card-4"><img src={karte4} alt="Karte4" /></div>
+          <div className="card-item card-5"><img src={karte5} alt="Karte5" /></div>
         </div>
       </div>
 
