37d7ffcd16a22a6439ac44e1a3a827d8a531b35f
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2025-07-16 16:33:07 +0200

    Add 3D rotating skill cards around avatar
    Introduced a 3D scene with three rotating skill cards orbiting around the avatar using new CSS animations and layout. Added new skill card images and updated App.js to render the 3D effect with proper structure and image imports.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Container } from 'react-bootstrap';
 import NavigationBar from './components/Navigation/Navbar';
 import avatarImage from './assets/images/Avatar.png'; // Beispiel für ein Avatar-Bild
+import karte1 from './assets/images/skillkarten/Alchemielabor.png';
+import karte2 from './assets/images/skillkarten/Wüstensand.png';
+import karte3 from './assets/images/skillkarten/GravityPlanet.png';
 import './App.css';
 
 function App() {
@@ -8,8 +11,20 @@ function App() {
     <>
       {/* Vollbild-Hintergrundbild */}
       <div className="fullscreen-bg"></div>
-      <img src={avatarImage} alt="Mein Avatar" className='avatar-center'></img>
       
+      {/* 3D-Szene mit Avatar und rotierenden Karten */}
+      <div className="rotating-cards">
+        <div className='card-orbit'>
+          {/* Avatar mit Wrapper für Gegen-Rotation */}
+          <div className="avatar-wrapper">
+            <img src={avatarImage} alt="Mein Avatar" className='avatar-center'></img>
+          </div>
+          <div className="card-item card-1"><img src={karte1} alt="Karte1" /></div>
+          <div className="card-item card-2"><img src={karte2} alt="Karte2" /></div>
+          <div className="card-item card-3"><img src={karte3} alt="Karte3" /></div>
+        </div>
+      </div>
+
       {/* Navigation */}
       <NavigationBar />
 
